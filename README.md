6060bb2c559c86b5bb0083bd5ba50c2a969e9941
Gilma Campos <gilmameister@gmail.com>
2021-04-19 17:58:32 -0300

    criando rotas get e post e verificando o funcionamento no Insomnia

---
+++ README.md
@@ -0,0 +1,2 @@
+Esta aplicação será construída a partir das aulas ministradas na trilha node.js 
+da Rocketseat na NLW#05 de abril de 2021.
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
-import express from "express";
+import express, { request, response } from "express";
 //adicionando a dependência de tipagem para o express
 const app = express();
+
+//criando rota
+app.get("/", (request, response) => {
+    return response.send("Olá MLW 05");
+});
+//criando rota post
+app.post("/users", (request, response) => {
+    return response.json({
+        message: "Usuário Cadastrado com Sucesso!"
+    });
+});
+
+
+
 //definindo a porta para o servidor
 app.listen(3335, () => console.log("O servidor está sendo executado na porta 3335"));
